74eb3e6f199f6af5f0c76845ae36920aeff7861d
MarcusReiners <137324174+MarcusReiners@users.noreply.github.com>
2025-11-28 11:24:34 +0100

    Create slack.js

---
+++ utils/slack.js
@@ -0,0 +1,9 @@
+import fetch from "node-fetch";
+
+export async function sendToSlack(message) {
+  await fetch(process.env.SLACK_WEBHOOK_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ text: message })
+  });
+}
